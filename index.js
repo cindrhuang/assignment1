fca129dbdb443aadff07e64e2c4f9ee81db0a5f6
cindrhuang <cindyrhuang@cmail.carleton.ca>
2021-10-06 16:08:24 -0400

    expanded to include more foods

--- index.js
+++ index.js
@@ -11,15 +11,29 @@ app.get('/', (req, res) => {
 //EGGS
 app.get('/api/eggs', (req, res) => {
     //dummy data
-    const eggs = ["Hard Boiled", "Soft Boiled", "Poached", "Sunny-side Up", "Omelette"];
+    const eggs = ["Hard  Egg", "Soft Boiled Egg", "Poached Egg", "Sunny-side Up Egg", "Omelette"];
     res.send(JSON.stringify(eggs));
 });
 
 //POTATOES
 app.get('/api/potatoes', (req, res) => {
     //dummy data
-    const potatoes = ["Baked", "Mashed", "Fries", "Tater Tots", "Hashbrown"];
+    const potatoes = ["Baked Potato", "Mashed Potatoes", "Fries", "Tater Tots", "Hashbrowns"];
     res.send(JSON.stringify(potatoes));
+});
+
+//NOODLES
+app.get('/api/noodles', (req, res) => {
+    //dummy data
+    const noodles = ["Pasta", "Hand-pulled Noodles", "Instant Noodles", "Rice Noodles", "Fried Noodles"];
+    res.send(JSON.stringify(noodles));
+});
+
+//STRAWBERRIES
+app.get('/api/strawberries', (req, res) => {
+    //dummy data
+    const strawberries = ["Strawberry Tarts", "Strawberry Milkshake", "Chocolate Covered Strawberries", "Strawberry Shortcake", "Strawberry Ice-cream"];
+    res.send(JSON.stringify(strawberries));
 });
 
 app.listen(port, () => {
